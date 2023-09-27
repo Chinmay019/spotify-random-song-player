1a6f0b4cd43cf43165918f2a045b2bd3f5507dae
Chinmay019 <chinmaynaik019@gmail.com>
2023-09-28 00:11:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Code from "./components/Code";
+import Home from "./components/Home/Home";
+import RandomSelect from "./components/RandomSelect";
+import { SpotifyProvider } from "./context/SpotifyContext";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Trending from "./components/Pages/Trending";
+import Playlist from "./components/Playlists/Playlists";
+
+function App() {
+  return (
+    <SpotifyProvider>
+      <Router>
+        <div className="main-container">
+          <Sidebar />
+          <div className="page-container">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/playlists" element={<Playlist />} />
+              <Route path="/trending" element={<Trending />} />
+              <Route path="/callback?/:code" element={<Code />} />
+            </Routes>
+          </div>
+          {/* <RandomSelect /> */}
+        </div>
+      </Router>
+    </SpotifyProvider>
+  );
+}
+
+export default App;
