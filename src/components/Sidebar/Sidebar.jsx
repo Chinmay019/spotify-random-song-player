f4b126b9a89eee73ad761f1a5dc053e00933e3a9
Chinmay019 <chinmaynaik019@gmail.com>
2023-09-28 01:18:02 +0530

    username styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Sidebar.css";
 import { BsFire, BsFillPlayCircleFill } from "react-icons/bs";
 import { BiSolidPlaylist } from "react-icons/bi";
 import { FaSignOutAlt } from "react-icons/fa";
-import { Link } from "react-router-dom";
 import SidebarButton from "./SidebarButton";
+import SpotifyContext from "../../context/SpotifyContext";
 
 function Sidebar() {
+  const { profileInfo } = useContext(SpotifyContext);
+  console.log(profileInfo, "from sidebar");
+
   return (
     // <div className="fixed top-16 w-72 flex px-4 py-5 overflow-auto h-full">
     <div className="sidebar-container">
-      <div>
+      <div className="profilepic-container">
         <img
           src="https://www.freecodecamp.org/news/content/images/2022/02/arrows-2889040_1920.jpg"
           alt="profile-picture"
           className="profile-pic"
         ></img>
-        <span>UserName</span>
+        <span className="username">{profileInfo?.display_name}</span>
       </div>
       <div className="links-container">
         <SidebarButton title="Trending" to="/trending" icon={<BsFire />} />
